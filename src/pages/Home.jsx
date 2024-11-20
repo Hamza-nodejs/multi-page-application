994cab59f1a9da3b47efa62d3fe91c610fdefb71
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-11-20 10:43:40 +0500

    uuid integration

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import NavBar from "../components/NavBar"
+import { v4 } from "uuid"
 export default function Home() {
+    const id = v4();
+
     return (
         <>
             <NavBar />
+            <h1>
+                your unique session_id:
+            </h1>
+            <p>
+                {id}
+            </p>
+            <p>
+                Refresh for new id
+            </p>
         </>
     )
 }
